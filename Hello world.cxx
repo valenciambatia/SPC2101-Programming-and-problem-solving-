ee44c02d1c7b7656279cc97f6d5292026a087181
valenciambatia <valenciambatia@gmail.com>
2025-09-24 17:39:22 +0300

    Add files via upload

---
+++ Hello world.cxx
@@ -0,0 +1,13 @@
+/*
+Name:Valencia Mbatia
+Reg no:CT101/G/26466
+Description:Hello World program
+*/
+//pre processor directive
+#include<stdio.h>
+//main function
+int main( ){
+printf("Hello world");
+
+return 0;
+}
