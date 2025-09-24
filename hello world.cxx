06b0271ba770a4582100a28249315bede4653987
valenciambatia <valenciambatia@gmail.com>
2025-09-24 17:37:48 +0300

    Add files via upload

---
+++ hello world.cxx
@@ -0,0 +1,16 @@
+/*
+Name.Valencia 
+Reg no.26466
+Date.12
+Description.Hello world program
+*/
+#include <stdio.h>
+int main( ){
+	printf("i am Valencia Mbatia\n");
+	printf("i am a computer science student\n");
+	printf("i am in my first year program\n");
+	printf("i am in kirinyaga university\n");
+	printf("i am a begginer\n");
+	printf("coding is interesting\n");
+	return 0;
+}
