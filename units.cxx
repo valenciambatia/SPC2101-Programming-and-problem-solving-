40236e41d5de939992e63c99ec593ca1a40d63a9
valenciambatia <valenciambatia@gmail.com>
2025-09-26 12:04:49 +0300

    Add files via upload

---
+++ units.cxx
@@ -0,0 +1,21 @@
+/*Name:Valencia Mbatia
+Reg no:CT101/G/26466/25
+Desription:units im doing this semester
+*/
+//pre processor directive
+#include <stdio.h>
+//main function
+int main( ){
+ //Display
+printf("1.Computer Architecture.\n");
+printf("2.Analogue Electronics.\n");
+printf("3.Discrete Mathematics.\n");
+printf("4.Mathematics for science.\n");
+printf("5.Introduction to programming and problem solving.\n");
+printf("6.Communication skills.\n");
+printf("7.Fundamentals of ICT.\n");
+printf("8.Physics.");
+//program completed
+return 0;
+}
+ 	 	
